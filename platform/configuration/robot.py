8d340c20ee409cce88d6b4b71db80ffb5765de73
ArtemZaZ <temka.911@mail.ru>
2020-01-14 14:16:34 +0300

    добавил обработчики для едубота

--- platform/configuration/robot.py
+++ platform/configuration/robot.py
@@ -1,14 +1,38 @@
 """ Конфигурация робота """
+from edubot import EduBot, MotorMode
+import smbus
+
+servoPosLen = 255
+middleServoPos = int(servoPosLen / 2)
+
+bus = smbus.SMBus(1)
+robot = EduBot(bus)
 
 
 def move(speed):
-    print("move", speed)
+    robot.setParrot0(int(-speed))
+    robot.setParrot1(int(speed))
 
 
-def turnForward(scale):
-    print("turnForward", scale)
+def rotate(speed):
+    robot.setParrot0(int(speed))
+    robot.setParrot1(int(speed))
 
 
-def rotate(scale):
-    print("rotate", scale)
+def setCamera(scale):
+    robot.beep()
+    robot.setServo0(middleServoPos - scale*servoPosLen)
 
+
+def initializeAll():
+    robot.online = True
+    robot.start()
+    robot.setMotorMode(MotorMode.MOTOR_MODE_PID)
+    robot.setParrot0(0)
+    robot.setParrot1(0)
+    robot.setServo0(int(middleServoPos))
+    robot.setServo1(int(middleServoPos))
+
+
+def release():
+    robot.exit()
